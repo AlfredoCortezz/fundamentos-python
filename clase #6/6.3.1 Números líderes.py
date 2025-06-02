5d94a89bcb123bb9c9ac825c3dae998cf01fe065
AlfredoCortezz <alfredocortezoff@gmail.com>
2025-06-02 07:44:58 -0600

    update ejercicio 6.3.1
    actualización de ejercicio 6.3.1

--- clase #6/6.3.1 Números líderes.py
+++ clase #6/6.3.1 Números líderes.py
@@ -1,13 +1,20 @@
-'''
-Clase:        Clase 6
-Tema:         Fortalecimeinto 
-Ejercicio:    6.2.1 Eliminando valores duplicados 
-Descripción:  Eliminar elementos duplicados
 
+'''
+clase:        clase 6
+tema:         fortalecimiento
+Descripción:  Números líderes 
 Autor:        Alfredo Enmanuel Cortez Martínez
 Fecha:        2025-05-30
 Estado:       [ Terminado ]
 '''
 
-lista = input("Ingresa tus datos: ").split()
-vacio = []
+numeros = list(map(int, input("Ingresa los números: ").split())) #estas funcionalidades las habiamos usado antes con Ish
+lideres = []
+
+mayor = -1
+for num in reversed(numeros):
+    if num > mayor:
+        lideres.append(num)
+        mayor = num
+
+print(*reversed(lideres))
