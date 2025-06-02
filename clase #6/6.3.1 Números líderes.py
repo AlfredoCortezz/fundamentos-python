7954c96a6037e82dcff1d72b19f38ceb93947456
AlfredoCortezz <alfredocortezoff@gmail.com>
2025-06-02 07:47:59 -0600

    ejercicio 6.3.1
    update

--- clase #6/6.3.1 Números líderes.py
+++ clase #6/6.3.1 Números líderes.py
@@ -1,14 +1,13 @@
-
 '''
-clase:        clase 6
-tema:         fortalecimiento
+Clase:        Clase 6
+Tema:         Fortalecimiento
 Descripción:  Números líderes 
 Autor:        Alfredo Enmanuel Cortez Martínez
 Fecha:        2025-05-30
 Estado:       [ Terminado ]
 '''
 
-numeros = list(map(int, input("Ingresa los números: ").split())) #estas funcionalidades las habiamos usado antes con Ish
+numeros = list(map(int, input("Ingresa los números: ").split()))  # estas funcionalidades las habíamos usado antes con Ish
 lideres = []
 
 mayor = -1
@@ -17,4 +16,5 @@ for num in reversed(numeros):
         lideres.append(num)
         mayor = num
 
-print("el numero lider es: " *reversed(lideres))
+lideres.reverse()  # Para mantener el orden original
+print("Los números líderes son:", *lideres)
